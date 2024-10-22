b5578828f7b99cd54ca7a308477d3d4fdf8379a1
scarabush_scarabush <snusikfrescus@gmail.com>
2024-10-22 18:01:42 +0300

    refactor hello

--- src/09.09.24/hello.cpp
+++ src/09.09.24/hello.cpp
@@ -14,14 +14,29 @@ int main() {
     
     
     findRoots(a, b, c);
+
+
     return EXIT_SUCCESS;
 }
 
 void findRoots(float a, float b, float c) {
-    float x1, x2;
+    float d, x1, x2;
     
-    x1 = (-b-sqrt((b*b)-4*a*c))/2*a;
-    x2 = (-b+sqrt((b*b)-4*a*c))/2*a;
+    d = (b*b) - (4*a*c);
+    if (d < 0) {
+        cout<<"no solution"<<endl;
+    } 
+
+    else if (d == 0) {
+        x1 = (-b)/(2*a);
+        cout<<"only one x = "<<x1<<endl;
+    } 
+
+    else {
+        x1 = (-b+sqrt(d))/(2*a); 
+        x2 = (-b-sqrt(d))/(2*a); 
+
+        cout<<"x1 = "<<x1<<", "<<"x2 = "<<x2<<endl;
+    }
     
-    cout<<"x1 = "<<x1<<", "<<"x2 = "<<x2<<endl;
 }
