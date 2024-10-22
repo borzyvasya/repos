8aa96732e576caa4d820888c4708178251ac137a
scarabush_scarabush <snusikfrescus@gmail.com>
2024-10-22 15:23:24 +0300

    adding tm

---
+++ src/09.09.24/architec/22.10/tm.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+
+
+using namespace std;
+
+bool containsB(const string& input) {
+    for (char c : input) {
+        if (c == 'b') {
+            return true;
+        }
+    }
+    return false; 
+}
+
+int main() {
+    string input;
+
+    cout << "Введите строку (состоящую из символов 'a' и 'b'): ";
+    cin >> input;
+
+    if (containsB(input)) {
+        cout << "Строка содержит хотя бы одну 'b'." << endl;
+    } else {
+        cout << "Строка не содержит 'b'." << endl;
+    }
+
+    return 0;
+}
