6fd0d7b98cfa7c78538f9ef23fa13e7ec6c3ce1c
scarabush_scarabush <snusikfrescus@gmail.com>
2024-10-28 15:01:56 +0300

    some refactoring

--- mak/diffarrays.cpp
+++ mak/diffarrays.cpp
@@ -3,25 +3,24 @@
 
 using namespace std;
 
-const int sizetwelve = 12;
+const int MAX_SIZE = 12;
 
 void formArrayC(int[], int[], int[], int);
 
 
 int main() {
-    int A[sizetwelve] {1, 2, 3, 4};
-    int B[sizetwelve] {5, 6, 7, 8};
-    int C[sizetwelve];
+    int A[MAX_SIZE] {1, 2, 3, 4, 1, 4, 6, 4};
+    int B[MAX_SIZE] {4, 6, 7, 8, 6, 1, 8, 4};
+    int C[MAX_SIZE];
 
-    int m = 4;
+    int m = 8;
 
     formArrayC(A, B, C, m);
 
     for (int i = 0; i < m; i++) {
-        cout<<C[i]<<"";
+        cout<<C[i]<<endl;
     }
 
-    cout<<endl;
 
 }
 
@@ -29,8 +28,5 @@ void formArrayC(int A[], int B[], int C[], int m) {
     for (int i = 0; i < m; i++) {
         C[i] = B[m - 1 - i] + A[i];
     }
+
 }
-
-
-
-
